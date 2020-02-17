e4e10c2c9908191d8d4e613a656875b19b952e53
ebuitragod <ebuitragod@gmail.com>
2020-02-17 11:01:29 -0500

    segundo commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ebuitragodicloud.com/Documents/08. Programs/00_Django/00_entorno_virtual/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
